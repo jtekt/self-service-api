3fa91738cbd43ef80982c7198dd2816a6093141c
Maxime Moreillon <m.moreillon@gmail.com>
2026-09-07 11:04:55 +0900

    icon

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,6 +17,9 @@ const fontSans = FontSans({
 export const metadata: Metadata = {
   title: "Self-service API",
   description: "Deploy a REST API from a postgresql database easily",
+  icons: {
+    icon: "/icon.png",
+  },
 };
 
 export default function RootLayout({
